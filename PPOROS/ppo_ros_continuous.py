4c7e4405918652be40b2b7d3b40ca26499934e82
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-02-15 15:17:02 -0600

    eval working with normalization

--- PPOROS/ppo_ros_continuous.py
+++ PPOROS/ppo_ros_continuous.py
@@ -1,5 +1,6 @@
 # docs and experiment results can be found at https://docs.cleanrl.dev/rl-algorithms/ppo/#ppo_continuous_actionpy
 import argparse
+import copy
 import os
 import random
 import time
@@ -18,6 +19,136 @@ from torch.utils.tensorboard import SummaryWriter
 from PPOROS.evaluate import Evaluate
 from PPOROS.utils import get_latest_run_id, parse_args
 
+
+class Evaluate:
+    """
+    Callback for evaluating an agent.
+
+    .. warning::
+
+      When using multiple environments, each call to  ``env.step()``
+      will effectively correspond to ``n_envs`` steps.
+      To account for that, you can use ``eval_freq = max(eval_freq // n_envs, 1)``
+
+    :param eval_env: The environment used for initialization
+    :param callback_on_new_best: Callback to trigger
+        when there is a new best model according to the ``mean_reward``
+    :param callback_after_eval: Callback to trigger after every evaluation
+    :param n_eval_episodes: The number of episodes to test the agent
+    :param eval_freq: Evaluate the agent every ``eval_freq`` call of the callback.
+    :param log_path: Path to a folder where the evaluations (``evaluations.npz``)
+        will be saved. It will be updated at each evaluation.
+    :param best_model_save_path: Path to a folder where the best model
+        according to performance on the eval env will be saved.
+    :param deterministic: Whether the evaluation should
+        use a stochastic or deterministic actions.
+    :param render: Whether to render or not the environment during evaluation
+    :param verbose: Verbosity level: 0 for no output, 1 for indicating information about evaluation results
+    :param warn: Passed to ``evaluate_policy`` (warns if ``eval_env`` has not been
+        wrapped with a Monitor wrapper)
+    """
+
+    def __init__(
+            self,
+            model,
+            eval_env,
+            n_eval_episodes: int = 5,
+            eval_freq: int = 10000,
+            log_path: str = None,
+            suffix: str = '',
+            best_model_save_path: str = None,
+            deterministic: bool = True,
+            device=None,
+    ):
+        self.n_eval_episodes = n_eval_episodes
+        self.eval_freq = eval_freq
+        self.best_mean_reward = -np.inf
+        self.last_mean_reward = -np.inf
+        self.deterministic = deterministic
+        self.device = device
+
+        self.model = model
+        self.eval_env = eval_env
+        self.best_model_save_path = log_path
+        self.suffix = suffix
+
+        # Logs will be written in ``evaluations.npz``
+        os.makedirs(name=log_path, exist_ok=True)
+        if log_path is not None:
+            if self.suffix != '':
+                self.log_path = os.path.join(log_path, f"evaluations_{suffix}")
+            else:
+                self.log_path = os.path.join(log_path, f"evaluations")
+        self.evaluations_returns = []
+        self.evaluations_timesteps = []
+        self.evaluations_successes = []
+        # For computing success rate
+        self._is_success_buffer = []
+
+    def evaluate(self, t, train_envs=None):
+        # if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+        returns, successes = self._evaluate(train_envs)
+
+        if self.log_path is not None:
+            self.evaluations_timesteps.append(t)
+            self.evaluations_returns.append(returns)
+            self.evaluations_successes.append(successes)
+
+            np.savez(
+                self.log_path,
+                timesteps=self.evaluations_timesteps,
+                returns=self.evaluations_returns,
+                successes=self.evaluations_successes,
+            )
+
+            mean_reward, std_reward = np.mean(returns), np.std(returns)
+            mean_success, std_success = np.mean(successes), np.std(successes)
+
+            self.last_mean_reward = mean_reward
+
+            print(f"Eval num_timesteps={t}, " f"episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
+            # print(f"Eval num_timesteps={t}, " f"episode_success={mean_success:.2f} +/- {std_success:.2f}")
+
+            if mean_reward > self.best_mean_reward:
+                print("New best mean reward!")
+                torch.save(self.model, os.path.join(self.best_model_save_path, "best_model"))
+                self.best_mean_reward = mean_reward
+
+    def _evaluate(self, train_envs):
+        eval_returns = []
+        eval_successes = []
+
+        obs, _ = self.eval_env.reset()
+        for episode_i in range(self.n_eval_episodes):
+            ep_returns = []
+            ep_successes = []
+            done = False
+            step = 0
+            while not done:
+                step += 1
+                # ALGO LOGIC: put action logic here
+                with torch.no_grad():
+                    if train_envs:
+                        obs = (obs - train_envs.obs_rms.mean) / np.sqrt(train_envs.obs_rms.var + 1e-8)
+                    actions = self.model.get_action(torch.Tensor(obs).to(self.device))
+                    # actions = self.model(torch.Tensor(obs).to(self.device))
+                    actions = actions.cpu().numpy().clip(self.eval_env.single_action_space.low,
+                                                         self.eval_env.single_action_space.high)
+
+                # TRY NOT TO MODIFY: execute the game and log data.
+                next_obs, rewards, terminateds, truncateds, infos = self.eval_env.step(actions)
+                done = terminateds[0] or truncateds[0]
+
+                # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
+                obs = next_obs
+
+                ep_returns.append(rewards[0])
+                ep_successes.append(terminateds[0])
+
+            eval_returns.append(np.sum(ep_returns))
+            eval_successes.append(np.sum(ep_successes) * 100)
+
+        return eval_returns, eval_successes
 def make_env(env_id, idx, capture_video, run_name, gamma):
     def thunk():
         if capture_video:
@@ -50,8 +181,8 @@ def make_eval_env(env_id, idx, capture_video, run_name, gamma):
             if idx == 0:
                 env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         env = gym.wrappers.ClipAction(env)
-        env = gym.wrappers.NormalizeObservation(env)
-        env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
+        # env = gym.wrappers.NormalizeObservation(env)
+        # env = gym.wrappers.TransformObservation(env, lambda obs: np.clip(obs, -10, 10))
         return env
 
     return thunk
@@ -170,24 +301,6 @@ def update_ppo(args, obs, logprobs, actions, advantages, returns, values):
     var_y = np.var(y_true)
     explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
 
-    # TRY NOT TO MODIFY: record rewards for plotting purposes
-    # writer.add_scalar("charts/learning_rate", optimizer.param_groups[0]["lr"], global_step)
-    writer.add_scalar("losses/value_loss", v_loss.item(), global_step)
-    writer.add_scalar("losses/policy_loss", pg_loss.item(), global_step)
-    writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
-    writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
-    writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
-    writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
-    writer.add_scalar("losses/explained_variance", explained_var, global_step)
-    # print("SPS:", int(global_step / (time.time() - start_time)))
-    # writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
-
-    if args.track and args.capture_video:
-        for filename in os.listdir(f"videos/{run_name}"):
-            if filename not in video_filenames and filename.endswith(".mp4"):
-                wandb.log({f"videos": wandb.Video(f"videos/{run_name}/{filename}")})
-                video_filenames.add(filename)
-
 if __name__ == "__main__":
     args = parse_args()
     if args.seed is None:
@@ -205,11 +318,7 @@ if __name__ == "__main__":
             # monitor_gym=True, no longer works for gymnasium
             save_code=True,
         )
-    writer = SummaryWriter(f"runs/{run_name}")
-    writer.add_text(
-        "hyperparameters",
-        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
-    )
+    writer = None
 
     # TRY NOT TO MODIFY: seeding
     random.seed(args.seed)
@@ -232,22 +341,40 @@ if __name__ == "__main__":
     run_id = get_latest_run_id(save_dir=save_dir) + 1
     save_dir += f"/run_{run_id}"
 
-    eval_envs = gym.vector.SyncVectorEnv(
-        [make_eval_env(args.env_id, i, args.capture_video, run_name, args.gamma) for i in range(args.num_envs)]
-    )
-    eval_module = Evaluate(model=agent, eval_env=eval_envs, n_eval_episodes=10, log_path=save_dir, device=device)
 
     # Save config
+    os.makedirs(save_dir, exist_ok=True)
     with open(os.path.join(save_dir, "config.yml"), "w") as f:
         yaml.dump(args, f, sort_keys=True)
 
+        # ROS behavior agent
+    agent_ros = copy.deepcopy(agent)  # initialize ros policy to be equal to the eval policy
+    optimizer_ros = optim.Adam(agent_ros.parameters(), lr=args.learning_rate_ros, eps=1e-5)
+
+    # Evaluation modules
+    eval_envs = gym.vector.SyncVectorEnv(
+        [make_eval_env(args.env_id, i, args.capture_video, run_name, args.gamma) for i in range(args.num_envs)]
+    )
+    eval_envs_ros = gym.vector.SyncVectorEnv(
+        [make_eval_env(args.env_id, i, args.capture_video, run_name, args.gamma) for i in range(args.num_envs)]
+    )
+    eval_module = Evaluate(model=agent, eval_env=eval_envs, n_eval_episodes=args.eval_episodes, log_path=args.save_dir,
+                           device=device)
+    eval_module_ros = Evaluate(model=agent_ros, eval_env=eval_envs_ros, n_eval_episodes=args.eval_episodes,
+                               log_path=args.save_dir, device=device, suffix='ros')
+
     # ALGO Logic: Storage setup
-    obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
-    actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
-    logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
-    values = torch.zeros((args.num_steps, args.num_envs)).to(device)
+    history_k = args.buffer_history
+    history_k = 1
+    buffer_size = history_k * args.num_steps
+    # ALGO Logic: Storage setup
+    # ALGO Logic: Storage setup
+    obs_buffer = torch.zeros((buffer_size, args.num_envs) + envs.single_observation_space.shape).to(device)
+    actions_buffer = torch.zeros((buffer_size, args.num_envs) + envs.single_action_space.shape).to(device)
+    rewards_buffer = torch.zeros((buffer_size, args.num_envs)).to(device)
+    dones_buffer = torch.zeros((buffer_size, args.num_envs)).to(device)
+
+    buffer_pos = 0
 
     # TRY NOT TO MODIFY: start the game
     global_step = 0
@@ -257,6 +384,9 @@ if __name__ == "__main__":
     next_done = torch.zeros(args.num_envs).to(device)
     num_updates = args.total_timesteps // args.batch_size
     video_filenames = set()
+
+    obs_dim = envs.single_observation_space.shape[0]
+    action_dim = envs.single_action_space.shape[0]
 
     # num_updates = 2
     for update in range(1, num_updates + 1):
@@ -268,20 +398,18 @@ if __name__ == "__main__":
 
         for step in range(0, args.num_steps):
             global_step += 1 * args.num_envs
-            obs[step] = next_obs
-            dones[step] = next_done
+            obs_buffer[step] = next_obs
+            dones_buffer[step] = next_done
 
             # ALGO LOGIC: action logic
             with torch.no_grad():
                 action, logprob, _, value = agent.get_action_and_value(next_obs)
-                values[step] = value.flatten()
-            actions[step] = action
-            logprobs[step] = logprob
+            actions_buffer[step] = action
 
             # TRY NOT TO MODIFY: execute the game and log data.
             next_obs, reward, terminated, truncated, infos = envs.step(action.cpu().numpy())
             done = np.logical_or(terminated, truncated)
-            rewards[step] = torch.tensor(reward).to(device).view(-1)
+            rewards_buffer[step] = torch.tensor(reward).to(device).view(-1)
             next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(done).to(device)
 
             # Only print when at least 1 env is done
@@ -293,8 +421,23 @@ if __name__ == "__main__":
                 if info is None:
                     continue
                 print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
-                writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+                # writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
                 # writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
+
+        if global_step < buffer_size:
+            indices = np.arange(buffer_pos)
+        else:
+            indices = (np.arange(buffer_size) + buffer_pos) % buffer_size
+
+        obs = obs_buffer.clone()[indices]
+        actions = actions_buffer.clone()[indices]
+        rewards = rewards_buffer.clone()[indices]
+        dones = dones_buffer.clone()[indices]
+
+        with torch.no_grad():
+            _, new_logprob, _, new_value = agent.get_action_and_value(obs.reshape(-1, obs_dim), actions.reshape(-1, action_dim))
+            values = new_value
+            logprobs = new_logprob.reshape(-1, 1)
 
         # bootstrap value if not done
         with torch.no_grad():
@@ -317,7 +460,7 @@ if __name__ == "__main__":
         if update % 1 == 0:
             current_time = time.time()-start_time
             print(f"Training time: {int(current_time)} \tsteps per sec: {int(global_step / current_time)}")
-            eval_module.evaluate(global_step)
+            eval_module.evaluate(global_step, train_envs=envs.envs[0])
 
     envs.close()
     writer.close()
