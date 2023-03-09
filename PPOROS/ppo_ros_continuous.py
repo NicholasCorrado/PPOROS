bc8a44bda3c74bf5dca0478a8b19dd3c5251bc48
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-03-09 11:14:28 -0600

    Clip actions

--- PPOROS/ppo_ros_continuous.py
+++ PPOROS/ppo_ros_continuous.py
@@ -148,17 +148,21 @@ class Agent(nn.Module):
     def get_value(self, x):
         return self.critic(x)
 
-    def get_action_and_value(self, x, action=None):
+    def get_action_and_value(self, x, action=None, clip=True):
         action_mean = self.actor_mean(x)
         action_logstd = self.actor_logstd.expand_as(action_mean)
         action_std = torch.exp(action_logstd)
         probs = Normal(action_mean, action_std)
         if action is None:
             action = probs.sample()
+        if clip:
+            action = action.clamp(min=-1, max=+1)
         return action, probs.log_prob(action).sum(1), probs.entropy().sum(1), self.critic(x)
 
-    def get_action(self, x):
+    def get_action(self, x, clip=True):
         action_mean = self.actor_mean(x)
+        if clip:
+            action_mean = action_mean.clamp(min=-1, max=+1)
         return action_mean
 
 def update_ppo(agent, optimizer, envs, obs, logprobs, actions, advantages, returns, values, args, global_step, writer):
