f582e0d97d57844bfa283b839aa9afcce9c96362
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-04-24 09:57:25 -0500

    hyperparams

---
+++ PPOROS/hyperparams/ppo_buffer.py
@@ -0,0 +1,102 @@
+PPOBUFFER_PARAMS = {
+    'Swimmer-v4': {
+        2: {
+            'num_steps': 4096,
+            'lr': 1e-3,
+            'target_kl': 0.03
+        },
+        4: {
+            'num_steps': 8192,
+            'lr': 1e-3,
+            'target_kl': 0.03,
+        },
+        8: {
+            'num_steps': 1024,
+            'lr': 1e-3,
+            'target_kl': 0.03,
+        },
+    },
+    'HalfCheetah-v4': {
+        2: {
+            'num_steps': 1024,
+            'lr': 1e-4,
+            'target_kl': 0.03,
+        },
+        4: {
+            'num_steps': 1024,
+            'lr': 1e-3,
+            'target_kl': 0.03,
+        },
+        8: {
+            'num_steps': 2048,
+            'lr': 1e-4,
+            'target_kl': 0.03,
+        },
+    },
+    'Hopper-v4': {
+        2: {
+            'num_steps': 2048,
+            'lr': 1e-4,
+            'target_kl': 0.03,
+        },
+        4: {
+            'num_steps': 2048,
+            'lr': 1e-4,
+            'target_kl': 0.03,
+        },
+        8: {
+            'num_steps': 1024,
+            'lr': 1e-3,
+            'target_kl': 0.03,
+        },
+    },
+    'Walker2d-v4': {
+        2: {
+            'num_steps': 4096,
+            'lr': 1e-3,
+            'target_kl': 0.03,
+        },
+        4: {
+            'num_steps': 2048,
+            'lr': 1e-3,
+            'target_kl': 0.03,
+        },
+        8: {
+            'num_steps': 8192,
+            'lr': 1e-4,
+            'target_kl': 0.03,
+        },
+    },
+    'Ant-v4': {
+        2: {
+            'num_steps': 2048,
+            'lr': 1e-4,
+            'target_kl': 0.03,
+        },
+        4: {
+            'num_steps': 4096,
+            'lr': 1e-4,
+            'target_kl': 0.03,
+        },
+        8: {
+            # 'num_steps': 8192,
+            # 'lr': 1e-4,
+        },
+    },
+    'Humanoid-v4': {
+        2: {
+            'num_steps': 8192,
+            'lr': 1e-4,
+            'target_kl': 0.03,
+        },
+        4: {
+            'num_steps': 4096,
+            'lr': 1e-3,
+            'target_kl': 0.03,
+        },
+        8: {
+            # 'num_steps': None,
+            # 'lr': None,
+        },
+    },
+}
