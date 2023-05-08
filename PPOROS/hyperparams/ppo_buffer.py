534cd3e9e8992217193dd1b28b8be6ca20f791a3
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-05-08 00:12:54 -0500

    ppo b params

--- PPOROS/hyperparams/ppo_buffer.py
+++ PPOROS/hyperparams/ppo_buffer.py
@@ -1,102 +1,86 @@
 PPOBUFFER_PARAMS = {
     'Swimmer-v4': {
-        2: {
+        1: {
             'num_steps': 4096,
             'lr': 1e-3,
-            'target_kl': 0.03
+        },
+        2: {
+            'num_steps': 2048,
+            'lr': 1e-3,
         },
         4: {
-            'num_steps': 4096,
+            'num_steps': 1024,
             'lr': 1e-3,
-            'target_kl': 0.03,
-        },
-        8: {
-            # 'num_steps': 1024,
-            # 'lr': 1e-3,
-            # 'target_kl': 0.03,
         },
     },
     'HalfCheetah-v4': {
-        2: {
+        1: {
             'num_steps': 1024,
             'lr': 1e-4,
-            'target_kl': 0.03,
         },
-        4: {
-            'num_steps': 1024,
-            'lr': 1e-4,
-            'target_kl': 0.03,
-        },
-        8: {
-            # 'num_steps': 2048,
-            # 'lr': 1e-4,
-            # 'target_kl': 0.03,
-        },
-    },
-    'Hopper-v4': {
-        2: {
-            'num_steps': 4096,
-            'lr': 1e-4,
-            'target_kl': 0.03,
-        },
-        4: {
-            'num_steps': 1024,
-            'lr': 1e-4,
-            'target_kl': 0.03,
-        },
-        8: {
-            # 'num_steps': 1024,
-            # 'lr': 1e-3,
-            # 'target_kl': 0.03,
-        },
-    },
-    'Walker2d-v4': {
         2: {
             'num_steps': 4096,
             'lr': 1e-3,
-            'target_kl': 0.03,
         },
         4: {
             'num_steps': 2048,
-            'lr': 1e-3,
-            'target_kl': 0.03,
+            'lr': 1e-4,
         },
-        8: {
-            # 'num_steps': 8192,
-            # 'lr': 1e-4,
-            # 'target_kl': 0.03,
+    },
+    'Hopper-v4': {
+        1: {
+            'num_steps': 2048,
+            'lr': 1e-3,
+        },
+        2: {
+            'num_steps': 1024,
+            'lr': 1e-4,
+        },
+        4: {
+            'num_steps': 1024,
+            'lr': 1e-4,
+        },
+    },
+    'Walker2d-v4': {
+        1: {
+            'num_steps': 4096,
+            'lr': 1e-3,
+        },
+        2: {
+            'num_steps': 4096,
+            'lr': 1e-3,
+        },
+        4: {
+            'num_steps': 1024,
+            'lr': 1e-3,
         },
     },
     'Ant-v4': {
-        2: {
-            'num_steps': 4096,
+        1: {
+            'num_steps': 1024,
             'lr': 1e-4,
-            'target_kl': 0.03,
+        },
+        2: {
+            'num_steps': 1024,
+            'lr': 1e-4,
         },
         4: {
-            'num_steps': 8192,
+            'num_steps': 4096,
             'lr': 1e-4,
-            'target_kl': 0.03,
-        },
-        8: {
-            # 'num_steps': 8192,
-            # 'lr': 1e-4,
         },
     },
     'Humanoid-v4': {
+        1: {
+            'num_steps': 8192,
+            'lr': 1e-4,
+        },
         2: {
             'num_steps': 8192,
             'lr': 1e-4,
-            'target_kl': 0.03,
         },
         4: {
             'num_steps': 4096,
             'lr': 1e-4,
-            'target_kl': 0.03,
-        },
-        8: {
-            # 'num_steps': None,
-            # 'lr': None,
         },
     },
 }
