d57989238623341f983a733f755a3f45b6748ec5
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-05-14 16:02:11 -0500

    relus

--- PPOROS/ppo_ros_continuous.py
+++ PPOROS/ppo_ros_continuous.py
@@ -652,17 +652,17 @@ def compute_se(args, agent, agent_ros, obs, actions, sampling_error_logs, global
     # agent_mle.actor_logstd = copy.deepcopy(agent.actor_logstd)
     agent_mle = copy.deepcopy(agent)
 
-    with torch.no_grad():
-        agent_mle.actor_logstd[:] = 0
+    # with torch.no_grad():
+    #     agent_mle.actor_logstd[:] = 0
     agent_mle.actor_logstd.requires_grad = False
     params = [p for p in agent_mle.actor_mean.parameters()]
-    params[0].requires_grad = False
-    params[2].requires_grad = False
+    # params[0].requires_grad = False
+    # params[2].requires_grad = False
     # agent_mle.actor_mean._modules['1'] = nn.ReLU()
     # agent_mle.actor_mean._modules['3'] = nn.ReLU()
 
-    # params[1] = nn.ReLU()
-    # params[3] = nn.ReLU()
+    params[1] = nn.ReLU()
+    params[3] = nn.ReLU()
 
 
     optimizer_mle = optim.Adam(agent_mle.parameters(), lr=args.se_lr)
