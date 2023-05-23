8d2973400f84de2cc41ee1e4616f09b53df05690
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-05-23 16:32:22 -0500

    all commands

--- PPOROS/cmds/rl_best_c0.1_b4/all.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for c in ['ho', 'w', 'hc', 's', 'a', 'hu']:
-# for c in [ 'w', 'hc', 's', 'ho']:
-    os.system(f'python {c}.py')
--- PPOROS/command_gen/rl_sweep/all.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# for c in ['ho', 'w', 'hc', 's', 'a', 'hu']:
-for c in ['ho', 'w', 'hc', 's', 'a', 'hu']:
-# for c in ['ho', 'hc', 's']:
-    os.system(f'python {c}.py')
-
-# for c in ['a','hu']:
-    # os.system(f'python {c}.py')
