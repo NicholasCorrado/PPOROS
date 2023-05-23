59a276bf961db9ba8d292cfaa4b153400d3424e2
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-05-23 13:47:03 -0500

    delete all that stuff I just committed

--- PPOROS/run_local_2.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if __name__ == "__main__":
-
-    for i in range(10):
-        os.system(f'python ppo_ros_discrete.py -f results_local --env-id CartPole-v1 --learning-rate 0 '
-                  f' --ros 0 -b 5 --total-timesteps {256*5} --eval-freq 1 --compute-sampling-error 1')
-
