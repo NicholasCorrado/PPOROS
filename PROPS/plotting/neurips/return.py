339ac2e4bc1649153aae5f8f232555a2034ca61b
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-07-05 12:19:48 -0500

    Neurips data and plotting scripts

---
+++ PROPS/plotting/neurips/return.py
@@ -0,0 +1,141 @@
+import os
+
+import seaborn
+from matplotlib import pyplot as plt
+
+from PPOROS.plotting.utils import get_paths, plot
+
+PROPS_PATHS = {
+    'Swimmer-v4': {
+        2: {
+            'path': 'data/rl/props/results/Swimmer-v4/ppo_ros/b_2/s_2048/s_1024/lr_0.001/lr_1e-05/kl_0.03/kl_0.03/l_0.1/e_16/mb_16/c_0.3/a_0',
+        }
+    },
+    'HalfCheetah-v4': {
+        2: {
+            'path': 'data/rl/props/results/HalfCheetah-v4/ppo_ros/b_2/s_1024/s_512/lr_0.0001/lr_0.001/kl_0.03/kl_0.05/l_0.3/e_16/mb_16/c_0.3/a_0',
+        }
+    },
+    'Hopper-v4': {
+        2: {
+            'path': 'data/rl/props/Hopper-v4/ppo_ros/b_2/s_2048/s_256/lr_0.001/lr_0.001/kl_0.03/kl_0.05/l_0.3/e_16/mb_16/c_0.3/a_0',
+        }
+    },
+    'Walker2d-v4': {
+        2: {
+            'path': 'data/rl/props/results/Walker2d-v4/ppo_ros/b_2/s_2048/s_256/lr_0.001/lr_0.001/kl_0.03/kl_0.1/l_0.3/e_16/mb_32/c_0.3/a_0',
+        }
+    },
+    'Ant-v4': {
+        2: {
+            'path': 'data/rl/props/results/Ant-v4/ppo_ros/b_2/s_2048/s_256/lr_0.0001/lr_0.001/kl_0.03/kl_0.03/l_0.1/e_16/mb_16/c_0.05/a_0',
+        }
+    },
+    'Humanoid-v4': {
+        2: {
+            'path': 'data/rl/props/results/Humanoid-v4/ppo_ros/b_2/s_8192/s_256/lr_0.0001/lr_0.0001/kl_0.03/kl_0.1/l_0.1/e_16/mb_32/c_0.3/a_0',
+        },
+    },
+}
+
+
+if __name__ == "__main__":
+
+    seaborn.set_theme(style='whitegrid')
+    env_ids = ['Swimmer-v4', 'Hopper-v4', 'HalfCheetah-v4', 'Walker2d-v4', 'Ant-v4', 'Humanoid-v4']
+
+    fig = plt.figure(figsize=(3*3,2*3))
+    i = 1
+    for env_id in env_ids:
+        ax = plt.subplot(2, 3, i)
+        i+=1
+
+        path_dict_all = {}
+
+        ### PROPS ##################################################################################################
+        key = rf'PROPS'
+        algo = 'ppo_ros'
+        results_dir = PROPS_PATHS[env_id][2]['path']
+        path_dict_aug = get_paths(
+            results_dir=results_dir,
+            key=key,
+            evaluations_name='evaluations')
+        path_dict_all.update(path_dict_aug)
+
+        ### PPO-PRIVILEGED #########################################################################################
+        key = rf'PPO-Privileged'
+        algo = 'ppo_buffer'
+        for s in [1024, 2048, 4096, 8192]:
+            for lr in [1e-3, 1e-4]:
+                for kl in [0.03]:
+                    results_dir = f'data/rl/ppo_privileged/results/{env_id}/{algo}/b_1/s_{s}/lr_{lr}/kl_{kl}/e_10'
+
+                    path_dict_aug = get_paths(
+                        results_dir=results_dir,
+                        key=key,
+                        x_scale=0.5,
+                        evaluations_name='evaluations')
+                    if len(path_dict_aug[key]['paths']) == 0: continue
+                    path_dict_all.update(path_dict_aug)
+
+
+        ### PPO-BUFFER #############################################################################################
+        key = rf'PPO-Buffer'
+        algo = 'ppo_buffer'
+        for s in [1024, 2048, 4096, 8192]:
+            for lr in [1e-3, 1e-4]:
+                for kl in [0.03]:
+                    results_dir = f'data/rl/ppo_buffer/results/{env_id}/{algo}/b_2/s_{s}/lr_{lr}/kl_0.03/e_10'
+
+                    path_dict_aug = get_paths(
+                        results_dir=results_dir,
+                        key=key,
+                        evaluations_name='evaluations')
+                    if len(path_dict_aug[key]['paths']) > 0:
+                        path_dict_all.update(path_dict_aug)
+
+        ### PPO ####################################################################################################
+        key = 'PPO'
+        algo = 'ppo_buffer'
+        for s in [1024, 2048, 4096, 8192]:
+            for lr in [1e-3, 1e-4]:
+                for kl in [0.03]:
+                    results_dir = f'data/rl/ppo_buffer/results/{env_id}/{algo}/b_1/s_{s}/lr_{lr}/kl_0.03/e_10'
+
+                    path_dict_aug = get_paths(
+                        results_dir=results_dir,
+                        key=key,
+                        evaluations_name='evaluations')
+                    if len(path_dict_aug[key]['paths']) > 0:
+                        path_dict_all.update(path_dict_aug)
+
+
+
+        plot(path_dict_all, name='returns')
+        plt.title(f'{env_id}', fontsize=20)
+        plt.xlabel('Timestep', fontsize=20)
+        plt.ylabel('Return', fontsize=20)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+
+        # Use scientific notation for x-axis
+        plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+        # set fontsize of 1e6
+        ax.xaxis.get_offset_text().set_fontsize(14)
+
+        plt.tight_layout()
+
+    # Push plots down to make room for the the legend
+    fig.subplots_adjust(top=0.85)
+
+    # Fetch and plot the legend from one of the subplots.
+    ax = fig.axes[0]
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper center', ncol=4, fontsize=17)
+
+    save_dir = f'figures'
+    save_name = f'return.png'
+    os.makedirs(save_dir, exist_ok=True)
+    plt.savefig(f'{save_dir}/{save_name}')
+
+    plt.show()
---
+++ PROPS/plotting/neurips/se_rl_one.py
@@ -0,0 +1,99 @@
+import os
+
+import numpy as np
+import seaborn
+from matplotlib import pyplot as plt
+
+from PPOROS.hyperparams.ppo import PPO_PARAMS
+from PPOROS.hyperparams.ppo_buffer import PPOBUFFER_PARAMS
+from PPOROS.plotting.ppo import BEST_PPO_PARAMS
+from PPOROS.plotting.utils import get_paths, load_data, plot, get_line_styles
+
+ylims = {
+    'Hopper-v4': (0, 4000),
+    'HalfCheetah-v4': (0, 4000),
+    'Walker2d-v4': (0, 400),
+    'Ant-v4': (0, 6000),
+    'InvertedPendulum-v4': (0, 1100),
+    'InvertedDoublePendulum-v4': (0, 10000),
+    'Acrobot-v1': (-500, 0),
+    'CartPole-v1': (0, 600),
+    'LunarLander-v2': (-250, 300),
+    'Swimmer-v4': (0, 150),
+    'Humanoid-v4': (0, 6500)
+}
+
+if __name__ == "__main__":
+
+    seaborn.set_theme(style='whitegrid')
+    env_ids = ['Humanoid-v4']
+
+
+
+
+    fig = plt.figure(figsize=(3, 3))
+    subplot_i = 0
+    for env_id in env_ids:
+        subplot_i += 1
+        plt.subplot(1, 1, subplot_i)
+
+        for num_steps in [1024,2048,4096,8192]:
+            for ros_num_steps in [256, 512, 1024, 2048, 4096]:
+                for lr in [1e-3, 1e-4]:
+                    for lr_ros in [1e-3,1e-4,1e-5]:
+                        for ros_lambda in [0.01, 0.1, 0.3]:
+                            for kl in [0.03, 0.05, 0.1]:
+                                for mb in [16,32]:
+                                    root_dir = f'./data/se_rl/results/{env_id}'
+                                    results_dir = f'{root_dir}/ppo_ros/b_2/s_{num_steps}/s_{ros_num_steps}/lr_{lr}/lr_{lr_ros}/kl_0.03/kl_{kl}/l_{ros_lambda}/e_16/mb_{mb}/c_0.3/a_0'
+
+                                    path_dict_all = {}
+                                    path_dict_all_ref = {}
+
+                                    ### PROPS SAMPLING ERROR ###########################################################
+                                    key = rf'PROPS'
+                                    path_dict_aug = get_paths(
+                                        results_dir=results_dir,
+                                        key=key,
+                                        evaluations_name='stats')
+                                    path_dict_all.update(path_dict_aug)
+                                    if len(path_dict_aug[key]['paths']) == 0:
+                                        continue
+
+                                    ### ON-POLICY SAMPLING ERROR #######################################################
+                                    key = rf'OS'
+                                    path_dict_aug = get_paths(
+                                        results_dir=results_dir,
+                                        key=key,
+                                        evaluations_name='stats')
+                                    path_dict_all_ref.update(path_dict_aug)
+                                    if len(path_dict_aug[key]['paths']) == 0:
+                                        continue
+
+                                    name = 'kl_mle_target'
+                                    name_ref = 'ref_kl_mle_target'
+
+                                    plot(path_dict_all, name=name) # PROPS SAMPLING ERROR
+                                    plot(path_dict_all_ref, name=name_ref) # ON-POLICY SAMPLING ERROR
+
+                                    plt.title(f'{env_id}', fontsize=16)
+                                    plt.xlabel('Timestep', fontsize=16)
+                                    plt.ylabel('Sampling Error', fontsize=16)
+                                    plt.xticks(fontsize=12)
+                                    plt.yticks(fontsize=12)
+                                    ax = fig.axes[0]
+                                    ax.xaxis.get_offset_text().set_fontsize(12)
+                                    plt.tight_layout()
+
+    fig.subplots_adjust(top=0.77)
+    ax = fig.axes[0]
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper center', ncol=5, fontsize=12)
+
+
+    save_dir = f'figures'
+    save_name = f'se_rl_one.png'
+    os.makedirs(save_dir, exist_ok=True)
+    plt.savefig(f'{save_dir}/{save_name}')
+
+    plt.show()
