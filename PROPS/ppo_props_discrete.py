c66208d6c50b31985e13a9d604e79cf5786673ab
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air-2.local>
2024-03-25 22:25:09 -0500

    remove print

--- PROPS/ppo_props_discrete.py
+++ PROPS/ppo_props_discrete.py
@@ -731,7 +731,7 @@ def main():
                     # print('On-policy sampling error:', sampling_error_logs[f'ref_kl_mle_target'])
 
                 sampling_error_logs['t'].append(global_step)
-                print('PROPS sampling error:', sampling_error_logs[f'kl_mle_target'])
+                # print('PROPS sampling error:', sampling_error_logs[f'kl_mle_target'])
                 np.savez(f'{args.save_dir}/stats.npz',
                          **sampling_error_logs)
 
