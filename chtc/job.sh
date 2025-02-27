62198a415a3423885b72e6d5a923c30b5d5d4073
Nicholas Corrado <ncorrado@wisc.edu>
2025-02-26 20:57:29 -0600

    update chtc

--- chtc/job.sh
+++ chtc/job.sh
@@ -1,51 +1,26 @@
 #!/bin/bash
 
-# have job exit if any command returns with non-zero exit status (aka failure)
-set -e
-
-pid=${1}
-step=${2}
-cmd=`tr '*' ' ' <<< $3`
+pid=$1  # ranges from 0 to num_commands*num_jobs-1 
+step=$2 # ranges from 0 to num_jobs-1
+cmd=`tr '*' ' ' <<< $3` # replace * with space
+cmd="${cmd} --run_id ${step} --seed ${step}"
 echo $cmd
 
+# fetch your code from /staging/
 CODENAME=PROPS
-export PATH
-
 cp /staging/ncorrado/${CODENAME}.tar.gz .
-tar -xzf ${CODENAME}.tar.gz -C .
-rm ${CODENAME}.tar.gz # remove code tarball
-cd $CODENAME
+tar -xzf ${CODENAME}.tar.gz
+rm ${CODENAME}.tar.gz
 
-# install code
-#ENVNAME=props
-#ENVDIR=${ENVNAME}
-#cp /staging/ncorrado/${ENVNAME}.tar.gz .
-#mkdir $ENVDIR
-#tar -xzf ${ENVNAME}.tar.gz -C $ENVDIR
-#rm ${ENVNAME}.tar.gz # remove env tarball
-#source $ENVDIR/bin/activate
-
-python3 -m venv env
-source env/bin/activate
-
-pip install torch --index-url https://download.pytorch.org/whl/cpu
-pip install stable-baselines3
-pip install tensorboard
-pip install "mujoco<3"
-pip install imageio
-pip install gymnasium
-pip install pyyaml
-
-pip install -e .
-pip install -e PROPS/custom-envs
 cd PROPS
+export PYTHONPATH=custom-envs:$PYTHONPATH # pip install -e fails on chtc because we don't have admin privileges .
+mkdir gymnasium_local
+python -m pip install 'gymnasium[mujoco]==0.29.1' --target gymnasium_local
+python -m pip install 'mujoco==2.3.5' --target mujoco_local
+export PYTHONPATH=gymnasium_local:$PYTHONPATH
+export PYTHONPATH=mujoco_local:$PYTHONPATH
+$cmd
 
-python3 -u ${cmd} --run-id ${step} --seed ${step}
-#$($cmd --seed $step --run-id $step)
-#$($command)
-
-tar -czvf results_${pid}.tar.gz results/*
-mv results_${pid}.tar.gz ../..
-
-cd ../..
-rm -rf $CODENAME
+# compress results. This file will be transferred to your submit node upon job completion.
+tar czvf results_${pid}.tar.gz results
+mv results_${pid}.tar.gz ..
